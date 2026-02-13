988c8f1f08666944732f75440ce59a5432d96c79
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-02-13 06:26:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your code here
+const inputPrice = document.getElementById("item-price-input");
+const inputName = document.getElementById("item-name-input");
+const total = document.getElementById("total");
+
+const tableBody = document.getElementById("table-body");
+const add = document.getElementById("add");
+
+const createTableData = (inputNameValue, inputPriceValue) => {
+  const row = document.createElement("tr");
+  const tdName = document.createElement("td");
+  const tdPrice = document.createElement("td");
+  tdName.textContent = inputNameValue;
+  tdPrice.textContent = inputPriceValue;
+  row.appendChild(tdName);
+  row.appendChild(tdPrice);
+  tableBody.appendChild(row);
+  let currentTotal = Number(total.textContent);
+  let price = Number(inputPriceValue);
+  total.textContent = currentTotal + price;
+};
+
+const addDataInTable = () => {
+  let inputPriceValue = inputPrice.value;
+  let inputNameValue = inputName.value;
+  createTableData(inputNameValue, inputPriceValue);
+  inputPrice.value = "";
+  inputName.value = "";
+};
+
+add.addEventListener("click", addDataInTable);
